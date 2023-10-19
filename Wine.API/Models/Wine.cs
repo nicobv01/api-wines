e48eb02ef153a0aac90446475df0263102f18aa3
nicobv01 <nicolbatista011@gmail.com>
2023-10-19 09:57:39 -0400

    add model wine

---
+++ Wine.API/Models/Wine.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wine.API.Models
+{
+    [Table("Wine")]
+    public class Wine
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? CountryCode { get; set; }
+        public int Type { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
