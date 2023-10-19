446c0c4848bca75c07f622e36944f8565a09e95c
nicobv01 <nicolbatista011@gmail.com>
2023-10-19 09:50:57 -0400

    add sql file

---
+++ SQLWine.sql
@@ -0,0 +1,18 @@
+/****** Object:  Table [dbo].[Wine]    Script Date: 19/10/2023 9:48:25 a. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Wine](
+	[Id] [bigint] NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Description] [varchar](500) NULL,
+	[CountryCode] [char](3) NOT NULL,
+	[Type] [int] NULL,
+	[Year] [Date] NULL
+) ON [PRIMARY]
+GO
+
+
