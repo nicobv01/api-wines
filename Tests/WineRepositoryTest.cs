1095a876ac80ec515dd4e3ae21f13e6550c40364
nicobv01 <nicolbatista011@gmail.com>
2023-10-20 15:39:52 -0400

    testing method insert wine

--- Tests/WineRepositoryTest.cs
+++ Tests/WineRepositoryTest.cs
@@ -1,14 +1,39 @@
-﻿using Xunit;
+﻿using API.Data;
+using API.Models;
+using API.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace Tests
 {
     public class WineRepositoryTest
     {
         [Fact]
-        public void example_test()
+        public async Task InsertWine_CheckInsertedData_Success()
         {
-               Assert.Equal(2, 1 + 1);
-        }
+            int id = 123;
+            DbContextOptions<AppDbContext> options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "Wine")
+                .Options;
 
+            var result = new Wine();
+
+            using (var context = new AppDbContext(options))
+            {
+                var wineRepository = new WineRepository(context);
+                var wine = new Wine
+                {
+                    Id = id,
+                    Name = "Test Wine",
+                    Description = "Test Wine",
+                    CountryCode = "RD",
+                    Type = 1
+                };
+                result = await wineRepository.Insert(wine);
+            }
+
+            Assert.Equal(id, result.Id);
+
+        }
     }
 }
