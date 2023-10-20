6e64ef36aa0572f205e70f940c9f91923ecd61e8
nicobv01 <nicolbatista011@gmail.com>
2023-10-20 15:11:26 -0400

    example test

---
+++ Tests/WineRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class WineRepositoryTest
+    {
+        [Fact]
+        public void example_test()
+        {
+               Assert.Equal(2, 1 + 1);
+        }
+
+    }
+}
