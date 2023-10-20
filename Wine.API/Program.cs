e5f4e114c85c6cea9cf59127c1367b49437d136e
nicobv01 <nicolbatista011@gmail.com>
2023-10-20 09:35:55 -0400

    update program.cs

--- Wine.API/Program.cs
+++ Wine.API/Program.cs
@@ -6,7 +6,7 @@ using API.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IWineRepository, WineRepository>();
+builder.Services.AddScoped<WineRepository>();
 builder.Services.AddScoped<IWineService, WineService>();
 builder.Services.AddControllers();
 
